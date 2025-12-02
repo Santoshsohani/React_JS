bcaa2c906bedd8fced6018b6fcba68c19ee3611f
santosh sohani <santoshsohani16@gmail.com>
2025-12-02 23:53:26 +0530

    feat: integrate Tailwind CSS for styling and update dependencies
    - Added Tailwind CSS and its Vite plugin to the project.
    - Updated App component to use Tailwind CSS classes for styling.
    - Removed existing CSS styles in index.css and imported Tailwind CSS.
    - Updated package.json to include new dependencies.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import './App.css'
+
 const App = () => {
   return (
-    <div>
+    <div className="text-xl text-blue-500 border-2 border-blue-500 p-4 flex justify-center items-center h-screen">
       Hello World
     </div>
   )
